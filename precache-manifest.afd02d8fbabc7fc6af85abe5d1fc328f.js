b47f9d01e33fc5928721728688274d08d4ea4583
T1mka <timka2609@gmail.com>
2018-12-12 14:28:00 +0600

    Updates

---
+++ precache-manifest.afd02d8fbabc7fc6af85abe5d1fc328f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a2d029805678f87b039",
+    "url": "/gpa-calculator/static/js/runtime~main.4a2d0298.js"
+  },
+  {
+    "revision": "6edddb71440f156e5290",
+    "url": "/gpa-calculator/static/js/main.6edddb71.chunk.js"
+  },
+  {
+    "revision": "7a78594346643c8207e8",
+    "url": "/gpa-calculator/static/js/1.7a785943.chunk.js"
+  },
+  {
+    "revision": "6edddb71440f156e5290",
+    "url": "/gpa-calculator/static/css/main.cc389615.chunk.css"
+  },
+  {
+    "revision": "7a78594346643c8207e8",
+    "url": "/gpa-calculator/static/css/1.0fcb9447.chunk.css"
+  },
+  {
+    "revision": "02bfec65552e52f101761e10a017a5d6",
+    "url": "/gpa-calculator/index.html"
+  }
+];
